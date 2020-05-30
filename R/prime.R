8fc2705bbbb56bced54b41695f96419c38647e55
yujeong <s21810687@stat01.yu.ac.kr>
2020-05-30 15:04:15 +0900

    checking prime number function

---
+++ R/prime.R
@@ -0,0 +1,20 @@
+#' Checking prime number
+#'
+#' This function checks prime numbers.
+#'
+#' @examples
+#'
+#' myprime(11)
+myprime <- function(x) {
+  if (x == 1) {
+    x_is_prime <- FALSE
+  } else if (x == 2) {
+    x_is_prime <- TRUE
+  } else {
+    x_is_prime <- TRUE
+    for (n in 2:(x/2)) {
+      if (x %% n == 0) x_is_prime <- FALSE
+    }
+  }
+  return(x_is_prime)
+}
